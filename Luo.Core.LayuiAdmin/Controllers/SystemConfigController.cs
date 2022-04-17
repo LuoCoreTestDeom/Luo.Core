5754b2b5258e8876e6990aafa643e907ab6c1e48
LuoCore <LuoCore@hotmail.com>
2022-04-17 21:48:26 +0800

    修改菜单，增加用户管理模块

---
+++ Luo.Core.LayuiAdmin/Controllers/SystemConfigController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Luo.Core.LayuiAdmin.Controllers
+{
+    public class SystemConfigController : Controller
+    {
+        public IActionResult UserManage()
+        {
+            return View();
+        }
+    }
+}
