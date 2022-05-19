d576dd097c458ca734f1c017c7a3fb152e9869ad
LuoCore <LuoCore@hotmail.com>
2022-05-19 22:44:58 +0800

    666

--- Luo.Core.VueWeb/vite.config.ts
+++ Luo.Core.VueWeb/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     plugins: [vue()],
     server: {
         host: '0.0.0.0',//½â¾övite   use `--host` to expose
-        port: 8080,
+        port: 1337,
         open: true
     },
     resolve: {
