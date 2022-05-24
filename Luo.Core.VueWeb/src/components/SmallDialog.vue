6b2a63ce92d3b380ddd2fb9d25917cfedea5c4f3
LuoCore <LuoCore@hotmail.com>
2022-05-24 22:06:30 +0800

    123

--- Luo.Core.VueWeb/src/components/SmallDialog.vue
+++ Luo.Core.VueWeb/src/components/SmallDialog.vue
@@ -11,7 +11,23 @@
     });
 </script>
 <template>
-    <div class="modal modal-blur fade show" tabindex="-1" role="dialog" aria-modal="true" v-show="isShow">
+
+<div class="modal modal-blur fade show" id="modal-small" tabindex="-1" style="display: block;" aria-modal="true" role="dialog">
+      <div class="modal-dialog modal-sm modal-dialog-centered" role="document">
+        <div class="modal-content">
+          <div class="modal-body">
+            <div class="modal-title">Are you sure?</div>
+            <div>If you proceed, you will lose all your personal data.</div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-link link-secondary me-auto" data-bs-dismiss="modal">Cancel</button>
+            <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Yes, delete all my data</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="modal modal-blur fade show" tabindex="-1" role="dialog" aria-modal="true" :style="{'display':isShow?'block':'none'}">
         <div class="modal-dialog modal-sm modal-dialog-centered" role="document">
             <div class="modal-content">
                 <div class="modal-body">
@@ -21,10 +37,12 @@
                 <div class="modal-footer">
                     <button type="button" class="btn btn-link link-secondary me-auto"
                         data-bs-dismiss="modal">Cancel</button>
-                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Yes, delete all my
-                        data</button>
                 </div>
             </div>
         </div>
     </div>
+
+
+
+
 </template>
--- Luo.Core.VueWeb/src/views/Login.vue
+++ Luo.Core.VueWeb/src/views/Login.vue
@@ -1,71 +1,71 @@
 <template>
-    
-    <div  class=" border-top-wide border-primary d-flex flex-column">
-        <div class="page page-center">
-          <div class="container-tight py-4">
-            <div class="text-center mb-4">
-              <a href="." class="navbar-brand navbar-brand-autodark"><img src="../assets/logo.png" height="36" alt=""></a>
-            </div>
-            <form class="card card-md" action="." method="get" autocomplete="off">
-              <div class="card-body">
-                <h2 class="card-title text-center mb-4">会员中心</h2>
-                <div class="mb-3">
-                  <label class="form-label">用户名</label>
-                  <input type="email" class="form-control" placeholder="用户名" autocomplete="off">
-                </div>
-                <div class="mb-2">
-                  <label class="form-label">
-                    密码
-                    <span class="form-label-description">
-                      <a href="./forgot-password.html">我忘记了密码</a>
-                    </span>
-                  </label>
-                  <div class="input-group input-group-flat">
-                    <input type="密码" class="form-control"  placeholder="密码"  autocomplete="off">
-                    <span class="input-group-text">
-                      <a href="#" class="link-secondary" title="Show password" data-bs-toggle="tooltip">
-                      </a>
-                    </span>
-                  </div>
-                </div>
-                <div class="mb-2">
-                  <label class="form-check">
-                    <input type="checkbox" class="form-check-input"/>
-                    <span class="form-check-label">记住密码</span>
-                  </label>
-                </div>
-                <div class="form-footer">
-                  <button type="button" class="btn btn-primary w-100" @click="plusOne()">登录</button>
-                </div>
-              </div>
-              
-            </form>
-            <div class="text-center text-muted mt-3">
-              还没有账号? <a href="./sign-up.html" tabindex="-1">注册{{isShow}}</a>
-            </div>
-            <h1 v-show="v-show">是否展示</h1>
-           <SmallDialog :title="dialogTitle" :isShow="isShow"></SmallDialog>
-          </div>
+
+  <div class=" border-top-wide border-primary d-flex flex-column">
+    <div class="page page-center">
+      <div class="container-tight py-4">
+        <div class="text-center mb-4">
+          <a href="." class="navbar-brand navbar-brand-autodark"><img src="../assets/logo.png" height="36" alt=""></a>
         </div>
-  
+        <form class="card card-md" action="." method="get" autocomplete="off">
+          <div class="card-body">
+            <h2 class="card-title text-center mb-4">会员中心</h2>
+            <div class="mb-3">
+              <label class="form-label">用户名</label>
+              <input type="email" class="form-control" placeholder="用户名" autocomplete="off">
+            </div>
+            <div class="mb-2">
+              <label class="form-label">
+                密码
+                <span class="form-label-description">
+                  <a href="./forgot-password.html">我忘记了密码</a>
+                </span>
+              </label>
+              <div class="input-group input-group-flat">
+                <input type="密码" class="form-control" placeholder="密码" autocomplete="off">
+                <span class="input-group-text">
+                  <a href="#" class="link-secondary" title="Show password" data-bs-toggle="tooltip">
+                  </a>
+                </span>
+              </div>
+            </div>
+            <div class="mb-2">
+              <label class="form-check">
+                <input type="checkbox" class="form-check-input" />
+                <span class="form-check-label">记住密码</span>
+              </label>
+            </div>
+            <div class="form-footer">
+              <button type="button" class="btn btn-primary w-100" @click="plusOne()">登录</button>
+            </div>
+          </div>
+
+        </form>
+        <div class="text-center text-muted mt-3">
+          还没有账号? <a href="./sign-up.html" tabindex="-1">注册{{ isShow }}</a>
+        </div>
+        <h1 v-show="isShow">是否展示</h1>
+        <SmallDialog :title="dialogTitle" :isShow="isShow"></SmallDialog>
+      </div>
     </div>
+
+  </div>
 
 </template>
 
 <script setup lang="ts">
-  import {ref,reactive} from 'vue'
-  
-  import axios from 'axios';
+import { ref, reactive } from 'vue'
 
- import SmallDialog from '@/components/SmallDialog.vue'
+import axios from 'axios';
 
- let dialogTitle=ref<String>("传值");
-  let  isShow=ref<Boolean>(false);
-    
-  function plusOne(){
-    isShow.value=true;
-    dialogTitle.value="111阿斯弗";
-    //debugger;
+import SmallDialog from '@/components/SmallDialog.vue'
+
+let dialogTitle = ref<String>("传值");
+let isShow = ref<Boolean>(false);
+
+function plusOne() {
+  isShow.value = !isShow.value;
+  dialogTitle.value = "111阿斯弗";
+  //debugger;
   //  const reqData={account: 'zhangsan', password: '123456789'};
   //   axios({
   //     method:"post",
@@ -83,6 +83,6 @@
   //     debugger;
   //       console.log("请求失败："+result.response.status+","+result.response.statusText);
   //   })
-    
-  }
+
+}
 </script>
