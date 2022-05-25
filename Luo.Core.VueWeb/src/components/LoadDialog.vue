d44f2ff334df4407fb58aafa9bf199d001dec3fc
LuoCore <LuoCore@hotmail.com>
2022-05-25 18:28:38 +0800

    123

--- Luo.Core.VueWeb/src/components/LoadDialog.vue
+++ Luo.Core.VueWeb/src/components/LoadDialog.vue
@@ -13,11 +13,9 @@
     <div class="modal fade modal-blur show" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
         role="dialog" :style="{'display':isShow?'block':'none'}">
         <div class="modal-dialog  modal-dialog-centered" role="document">
-            <div class="modal-content" style="text-align: center;display: inline;">
-                <div class="spinner-grow" style="width: 5rem; height: 5rem;" role="status">
-                    <span class="visually-hidden">Loading...</span>
-                </div>
-            </div>
+          <div class="spinner-grow modal-content" style="width: 5rem; height: 5rem;" role="status">
+            <span class="visually-hidden">Loading...</span>
+        </div>
         </div>
     </div>
 </template>
